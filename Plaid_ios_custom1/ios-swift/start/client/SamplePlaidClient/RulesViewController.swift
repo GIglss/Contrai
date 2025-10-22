fdb24cb91bcb4ea62280b1eb33c9071f0ccf47b4
glglss <gon.iglesias.g@gmail.com>
2025-10-22 16:38:07 +0200

    just title to Rules view

--- Plaid_ios_custom1/ios-swift/start/client/SamplePlaidClient/RulesViewController.swift
+++ Plaid_ios_custom1/ios-swift/start/client/SamplePlaidClient/RulesViewController.swift
@@ -15,9 +15,29 @@ class RulesViewController: UIViewController {
         super.viewDidLoad()
         
         // Set the title for the navigation bar
-        self.title = "Rules"
+        self.title = "Flow Rules"
         
         // Set the tab bar item
         self.tabBarItem = UITabBarItem(title: "Rules", image: UIImage(systemName: "list.bullet.rectangle"), tag: 1)
+        
+        setupBasicUI()
+    }
+    
+    private func setupBasicUI() {
+        // Add subtitle label
+        let subtitleLabel = UILabel()
+        subtitleLabel.text = "Automate your money transfers"
+        subtitleLabel.font = UIFont.systemFont(ofSize: 16)
+        subtitleLabel.textColor = .systemGray
+        subtitleLabel.textAlignment = .center
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(subtitleLabel)
+        
+        NSLayoutConstraint.activate([
+            subtitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            subtitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
     }
 }
