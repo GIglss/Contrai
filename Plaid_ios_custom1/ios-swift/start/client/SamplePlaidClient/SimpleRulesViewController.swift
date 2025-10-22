87266c2d2721ae475b8f0a70a379e14d1e157e51
glglss <gon.iglesias.g@gmail.com>
2025-10-22 17:26:34 +0200

    new test to enhance Rules

--- Plaid_ios_custom1/ios-swift/start/client/SamplePlaidClient/SimpleRulesViewController.swift
+++ Plaid_ios_custom1/ios-swift/start/client/SamplePlaidClient/SimpleRulesViewController.swift
@@ -8,25 +8,5 @@
 import UIKit
 
 class SimpleRulesViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        print("📱 SimpleRulesViewController loaded successfully!")
-        
-        view.backgroundColor = .systemBackground
-        self.title = "Flow Rules"
-        
-        let label = UILabel()
-        label.text = "Simple Rules Controller Works!"
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(label)
-        
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-    }
+    // Completely empty - just inherits from UIViewController
 }
